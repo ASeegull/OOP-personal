3e67047107cf4e4c04eea0f9afd133f26bc76c0c
Anna <a.chaikalemberg@gmail.com>
2017-07-11 21:01:01 +0300

    Script for adding user data to table

--- scripts/people_table.js
+++ scripts/people_table.js
@@ -1,21 +1,28 @@
 var users = [];
-var user = null;
+var user;
 var fields = ['name', 'birth-date', 'address', 'phone', 'email'];
 function User() {
-  var values = [];
-  var name = '';
-  var birth = '';
-  var address = '';
-  var phone = '';
-  var email = '';
-  var sex = '';
-  this.id = '';
+  this.getFormValues = function () {
+    var values = [];
+    var i = 0;
+    for (i; i <= (fields.length - 1); i++) {
+      var lang = document.querySelector(`[name="${fields[i]}"]`).value;
+      values.push(lang);
+    }
+    lang = document.querySelector('[name="sex"]:checked').value;
+    values.push(lang);
+    this.setUserData(values);
+  };
+
+  this.setUserData = function (values) {
+    this.name = values[0];
+    this.birth_date = values[1];
+    this.address = values[2];
+    this.phone = values[3];
+    this.email = values[4];
+    this.sex = values[5];
+  };
 }
-
-
-User.prototype.getUserData = function (property) {
-  return property;
-};
 
 
 function saveUser(e) {
@@ -23,51 +30,37 @@ function saveUser(e) {
   user = new User();
   user.setId();
   user.getFormValues();
-  user.addCard();
-  users.push(user);
+  user.addTableRow();
   clearInput();
 }
 
 User.prototype.setId = function () {
- this.id = Math.floor(Math.random() * 1000);
+  this.id = Math.floor(Math.random() * 1000);
 };
 
-User.prototype.getFormValues = function () {
-  var values = [];
-  var i = 0;
-  for (i; i <= (fields.length - 1); i++) {
-    var lang = document.querySelector(`[name="${fields[i]}"]`).value;
-    values.push(lang);
-  }
-  lang = document.querySelector('[name="sex"]:checked').value;
-  values.push(lang);
-  user.setUserData(values);
+User.prototype.addTableRow = function () {
+  var $tbody = document.querySelector('tbody');
+  var createRow = document.createElement('tr');
+  createRow.setAttribute('data-id', this.id);
+  $tbody.appendChild(createRow);
+  var userData = [this.name, this.sex, this.birth_date, this.address, this.phone, this.email];
+  var $tr = document.querySelector('[data-id=\"' + this.id + '\"]');
+  userData.forEach(function (item) {
+    var $tcell = document.createElement('td');
+    $tcell.innerText = item;
+    $tr.appendChild($tcell);
+  });
 };
 
-User.prototype.setUserData = function (values) {
-  name = values[0];
-  birth = values[1];
-  address = values[2];
-  phone = values[3];
-  email = values[4];
-  sex = values[5];
-};
-
-User.prototype.addCard = function () {
-var $userList = document.querySelector('div.user-list');
-var $user = document.createElement('div');
-$user.id = user.id;
-$user.innerText = user.getUserData(name);
-  $userList.appendChild($user);
-};
 
 function clearInput () {
   var i = 0;
   for (i; i <= (fields.length - 1); i++) {
     document.querySelector(`[name="${fields[i]}"]`).value = "";
   }
-document.querySelector('[value="male"]').checked = true;
+  document.querySelector('[value="male"]').checked = true;
 }
 
 var $submit = document.querySelector('input[type="submit"]');
 $submit.addEventListener('click', saveUser, false);
+
